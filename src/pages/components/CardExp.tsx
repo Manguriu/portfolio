0a9804f569214db0bdb32023857e9a7f44cbac40
Manguriu <101741844+Manguriu@users.noreply.github.com>
2023-05-22 20:47:20 +0300

    Update CardExp.tsx

--- src/pages/components/CardExp.tsx
+++ src/pages/components/CardExp.tsx
@@ -1,54 +1,48 @@
-import React from 'react'
-import {motion} from "framer-motion"
+import React from 'react';
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 export default function CardExp({}: Props) {
   return (
-   <article className='flex flex-col rounded-l items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-50 cursor-pointer overflow-hidden transition-opacity  duration-3000'>
-    <motion.img 
-    initial={{
-      y: -100,
-      opacity:0
-    }}
-    transition={{
-      duration:1.5
-    }}
-    whileInView={{
-      opacity:1,
-      y:0,
-    }}
-    className='w-30 h-32 rounded-full  xl:w-[200px] xl:h-[200px] object-cover object-top'
-    src='pic.png' alt=''/>
+    <article className='flex flex-col rounded-l items-center space-y-4 flex-shrink-0 w-[200px] md:w-[300px] snap-center bg-[#292929] p-2 hover:opacity-100 opacity-50 cursor-pointer overflow-hidden transition-opacity duration-3000'>
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0
+        }}
+        transition={{
+          duration: 1.5
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        className='w-6 h-6 rounded-full object-cover object-top'
+        src='pic.png'
+        alt=''
+      />
 
-    <div className='px-0 md:px-10'>
-      <h4 className='text-4xl font-light'> Trainee </h4>
-      <p className='font-bold text-2xl mt-1'> TSMHS </p>
+      <div className='px-0'>
+        <h4 className='text-xs font-light'>Trainee</h4>
+        <p className='font-bold text-xs mt-1'>TSMHS</p>
 
+        <div className='flex space-x-1 my-1'>
+          <img className="h-3 w-3 rounded-full" src='react-original-wordmark.svg' alt='' />
+          <img className="h-3 w-3 rounded-full" src='typescript-original.svg' alt='' />
+          <img className="h-3 w-3 rounded-full" src='php-original.svg' alt='' />
+        </div>
 
-      <div className='flex space-x-2 my-2'>
-        <img  className="h-10 w-10 rounded-full" src='react-original-wordmark.svg' alt=''/>
-        <img  className="h-10 w-10 rounded-full" src='typescript-original.svg' alt=''/>
-        <img  className="h-10 w-10 rounded-full" src='php-original.svg'alt=''/>
-      
+        <p className='uppercase py-2 text-xs'>from... - to...</p>
+
+        <ul className='list-disc space-y-2 ml-1 text-xxs'>
+          <li>some points</li>
+          <li>some points</li>
+          <li>some points</li>
+          <li>some points</li>
+        </ul>
       </div>
-
-
-      <p className='uppercase py-5'> from... -to...</p>
-
-
-      <ul className='list-disc space-y-4 ml-5 text-lg'>
-        <li>some points</li>
-        <li>some points</li>
-        <li>some points</li>
-        <li>some points</li>
-      </ul>
-
-
-    </div>
-
-   </article>
-
+    </article>
   );
 }
 
