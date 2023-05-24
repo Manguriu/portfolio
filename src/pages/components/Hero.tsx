8df8d30e08dde4c7d57663f057a49558c8fec65d
Manguriu <101741844+Manguriu@users.noreply.github.com>
2023-05-24 23:36:05 +0300

    Update Hero.tsx

--- src/pages/components/Hero.tsx
+++ src/pages/components/Hero.tsx
@@ -1,55 +1,52 @@
 import Link from 'next/link';
-import React from 'react'
-import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import PopupCircles from './PopupCircles';
 import Progress from './Progress';
 
-
-
-type Props = {}
+type Props = {};
 
 function Hero({}: Props) {
-    const [text, helper] = useTypewriter({
-        words:[
-            "Manguriu Is The Name",
-            "I Like Programming <Npm install code loving!>",
-            "Coffe is my thing < git init love it />",
-        ],
-        loop:true,
-        delaySpeed:1000,
-    });
+  const [text, helper] = useTypewriter({
+    words: [
+      "Manguriu Is The Name",
+      "I Like Programming <Npm install code loving!>",
+      "Coffee is my thing < git init love it />",
+    ],
+    loop: true,
+    delaySpeed: 1000,
+  });
   return (
-    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
-        <PopupCircles />
-        <img className="relative rounded-full mx-auto object-cover" src='pic.png' alt='' />
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <PopupCircles />
+      <img className="relative rounded-full mx-auto object-cover" src="pic.png" alt="" />
 
-
-    <div className='z-20'>
-          <h2 className='text-sm uppercase text-red-300 pb-2 tracking-[15px]'>Software developer</h2>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-red-300 pb-2 tracking-[15px]">Software developer</h2>
         <h1>
-        <span className='text-red-300 text-2xl lg:text-4xl font-semibold px-5 '>{text}</span>
-        <Cursor cursorColor='red'></Cursor>
+          <span className="text-red-300 text-2xl lg:text-4xl font-semibold px-5">{text}</span>
+          <Cursor cursorColor="red" />
         </h1>
-        <div className='text-red-300 pt-7'>
+        <div className="text-red-300 pt-7">
           <Link href="#about">
-          <button className='px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[white] hover:text-[white]'>About</button>
+            <button className="px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[white] hover:text-[white]">About</button>
           </Link>
           <Link href="#experience">
-          <button className='px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[blue] hover:text-[blue]'>Experiecnce</button>
+            <button className="px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[blue] hover:text-[blue]">Experience</button>
           </Link>
-          <Link href="#skills">          
-          <button className='px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[red] hover:text-[red]'>Skills</button>
+          <Link href="#skills">
+            <button className="px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[red] hover:text-[red]">Skills</button>
           </Link>
           <Link href="#projects">
-          <button className='px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[#f700ff] hover:text-[#f700ff]'>Projects</button>
+            <button className="px-6 py-2 border border-[rgb(70,62,71)] rounded-full uppercase text-xm tracking-widest transition-all hover:border-[#f700ff] hover:text-[#f700ff]">Projects</button>
           </Link>
         </div>
-        </div>
-        <header className="text-center text-white py-1 bg-green-500 font-semibold"><Progress /></header>
-        
+      </div>
+      <header className="text-center text-white py-1 bg-green-500 font-semibold">
+        <Progress />
+      </header>
     </div>
-               
-  )
+  );
 }
 
-export default Hero
+export default Hero;
